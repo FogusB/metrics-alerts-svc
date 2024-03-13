82ae142766abc7950a133e881fde5c7861ba0689
Maksim Zebel <maksim.zebel@indev.by>
2024-03-14 02:28:04 +0300

    fix: test 1

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,5 +1,7 @@
 package main
 
+//go:generate go build -o=agent
+
 import (
 	"fmt"
 	"github.com/FogusB/metrics-alerts-svc/internal/collects"
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,7 @@
 package main
 
+//go:generate go build -o=server
+
 import (
 	"github.com/FogusB/metrics-alerts-svc/internal/handlers"
 	"github.com/FogusB/metrics-alerts-svc/internal/routers"
